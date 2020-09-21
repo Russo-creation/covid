e9dcf47579f0b9136ac8e0051ab434226c7e1e34
Łukasz Macoń <luk.macon@gmail.com>
2020-09-21 23:55:27 +0200

    Updates

---
+++ precache-manifest.773504a58cc80dd9fbbbf9667999b213.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e166af5b8372d508ff42e0685a3a0711",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "b450247fe54abc910933",
+    "url": "/covid/static/css/4.a60c9901.chunk.css"
+  },
+  {
+    "revision": "fc673846b3fe51af9d6b",
+    "url": "/covid/static/css/main.093eeb9a.chunk.css"
+  },
+  {
+    "revision": "e34a98410ff2fd3ac213",
+    "url": "/covid/static/js/2.4941e1d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid/static/js/2.4941e1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a565985cdf9cb2f26e47",
+    "url": "/covid/static/js/3.dd9db61d.chunk.js"
+  },
+  {
+    "revision": "bd777d96aa413f0a6ad86ace13baac09",
+    "url": "/covid/static/js/3.dd9db61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b450247fe54abc910933",
+    "url": "/covid/static/js/4.733de8a6.chunk.js"
+  },
+  {
+    "revision": "bd717da02e652aecd4bc",
+    "url": "/covid/static/js/5.571f2dec.chunk.js"
+  },
+  {
+    "revision": "fc673846b3fe51af9d6b",
+    "url": "/covid/static/js/main.68ef1613.chunk.js"
+  },
+  {
+    "revision": "ba493792aafe0c0acb72",
+    "url": "/covid/static/js/runtime-main.f11d01bc.js"
+  },
+  {
+    "revision": "678b2820d697bdc09c3de71db936fb35",
+    "url": "/covid/static/media/breathing.678b2820.png"
+  },
+  {
+    "revision": "18ba4029c7b294792fc0b23d60298b62",
+    "url": "/covid/static/media/cough.18ba4029.png"
+  },
+  {
+    "revision": "6cbc1c29012f1b86aa839b25241dd6d7",
+    "url": "/covid/static/media/covidBcg.6cbc1c29.jpg"
+  },
+  {
+    "revision": "1ffd85c9ff8fda45ee0890be658bdf07",
+    "url": "/covid/static/media/fever.1ffd85c9.png"
+  },
+  {
+    "revision": "c8c6ab516f2cee4458706fcc5fbbc8bf",
+    "url": "/covid/static/media/galaxy_starfield1.c8c6ab51.jpg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/covid/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "6f8c81e8e41e0bf315e025008176f354",
+    "url": "/covid/static/media/sore-throat.6f8c81e8.png"
+  },
+  {
+    "revision": "e486d5ae1dbba7837034a305d04b6495",
+    "url": "/covid/static/media/washHands.e486d5ae.jpg"
+  }
+]);
